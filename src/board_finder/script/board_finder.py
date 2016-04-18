f069d5efed9fc557c87ab562ecf66fb3517ce8ff
John Grossmann <jgrossmann@colgate.edu>
2016-04-18 19:58:22 -0400

    update

---
+++ src/board_finder/script/board_finder.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import rospy
+from sensor_msgs.msg import Image
+import cv2
+import numpy as np
+from cv_bridge import CvBridge, CvBridgeError
+
+done = False
+
+def callback(data):
+	print('callback')
+	print(rospy.get_caller_id() + " "+ str(data.header.stamp))
+	bridge = CvBridge()
+	
+	try:
+		img = bridge.imgmsg_to_cv2(data, "bgr8")
+	except CvBridgeError as e:
+		print(e)
+		
+	
+	try:
+		cv2.imwrite('image.jpg', img)
+	except:
+		print("Error writing image")
+		
+	done = True
+	
+	
+
+def listener():
+	rospy.init_node('camera_listener', anonymous=True)
+	rospy.Subscriber("/wide_stereo/left/image_color", Image, callback)
+	print("Starting listener")
+	while(done == False):
+		pass
+	
+if __name__ == '__main__':
+	listener()
---
+++ src/board_finder/src/point_cloud_convertor.cpp
@@ -0,0 +1,71 @@
+#include <ros/ros.h>
+#include <pcl_ros/point_cloud.h>
+#include <pcl/point_types.h>
+#include <boost/foreach.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include "opencv2/highgui/highgui.hpp"
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+void callback(const sensor_msgs::PointCloud2::ConstPtr& msg) {
+    ostringstream width;
+	width << msg->width;
+    ROS_INFO(width.str().c_str());
+
+    ostringstream height;
+	height << msg->height;
+    ROS_INFO(height.str().c_str());
+
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
+	pcl::fromROSMsg (*msg, *cloud);
+	
+    cloud->width = 640;
+	cloud->height = 480;
+	cloud->points.resize(cloud->width * cloud->height);
+
+    
+    
+	cv::Mat imageFrame;
+	if (cloud->isOrganized())
+	{
+        ROS_INFO("is organized");
+		imageFrame = cv::Mat(cloud->height, cloud->width, CV_8UC3); 
+		{
+
+		    for (int h=0; h<imageFrame.rows; h++) 
+		    {
+		        for (int w=0; w<imageFrame.cols; w++) 
+		        {
+		            pcl::PointXYZRGB point = cloud->at(w, h);
+
+		            Eigen::Vector3i rgb = point.getRGBVector3i();
+
+		            imageFrame.at<cv::Vec3b>(h,w)[0] = rgb[2];
+		            imageFrame.at<cv::Vec3b>(h,w)[1] = rgb[1];
+		            imageFrame.at<cv::Vec3b>(h,w)[2] = rgb[0];
+		        }
+		    }
+		}
+	}
+
+    vector<int> compression_params;
+    compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
+    compression_params.push_back(9);
+
+	cv::imwrite("kinect_image.png", imageFrame, compression_params);
+	ROS_INFO("wrote image");
+}
+
+int main(int argc, char** argv) {
+
+	ros::init(argc, argv, "point_cloud_convertor");
+	ros::NodeHandle nh;
+	ros::Subscriber sub = nh.subscribe(
+						"/head_mount_kinect/depth_registered/points",
+						2,
+						callback);
+	ros::spin();
+}
